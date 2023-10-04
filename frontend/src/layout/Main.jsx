14f9d8d5e66fc415d4a04cd29dbdee63b7e9fdd1
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2023-10-05 05:50:36 +0600

    folder structure added

--- frontend/src/layout/Main.jsx
+++ frontend/src/layout/Main.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/Navbar/NavBar";
+import Footer from "../components/Footer/Footer";
+
 const Main = () => {
     return (
         <div>
-            This is main Layout
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Offers from "../../components/Offers/Offers"
+import Trending from "../../components/Trending/Trending"
+
+const Home = () => {
+  return (
+    <div>
+        <Offers></Offers>
+        <Trending></Trending>
+    </div>
+  )
+}
+
+export default Home
