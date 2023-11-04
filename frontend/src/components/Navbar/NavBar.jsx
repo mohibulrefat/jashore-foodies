dd9dddb5245d66017d72dd884c0be19fe25accd3
nishattandra <nishattandra2001@gmail.com>
2023-11-04 22:09:31 +0600

    Navbar edited

--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -1,55 +1,87 @@
-import { BsCart3 } from "react-icons/bs"
-import { MdOutlineDarkMode } from "react-icons/md"
-
+import { useState } from "react";
+import {
+  PaperAirplaneIcon,
+  MoonIcon,
+  SunIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/outline";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div className="navbar bg-[#FFF8EE] font-[Roboto] text-slate-600  font-bold">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 w-52 text-sm">
-            <li><a>HOME</a></li>
-            <li> <a>RESTAURANTS</a> </li>
-            <li><a>FOOD ITEMS</a></li>
-            <li><a>MY DASHBOARD</a></li>
-          </ul>
+    <div className="bg-[#FFF8EE]">
+      <nav>
+        <div className="max-w-7xl mx-auto">
+          <div className="flex mx-auto justify-between w-full">
+            {/* Primary menu and logo */}
+            <div className="flex items-center gap-16 my-6">
+              {/* logo */}
+              <div>
+                <a
+                  href="/"
+                  className="flex gap-1 font-bold text-gray-700 items-center "
+                >
+                  <PaperAirplaneIcon className="h-6 w-6 text-primary" />
+                  <span>JASHORE FOODIES</span>
+                </a>
+              </div>
+              {/* primary */}
+              <div className="hidden lg:flex gap-8 lg:ml-40">
+                <NavLink to='/' className={({ isActive }) => (isActive ? 'text-lg' : 'border-b border-[#E94339] hover:bg-[#E94339]')}>Home</NavLink>
+                <a href="#">RESTAURANTS</a>
+                <a href="#">FOOD ITEMS</a>
+                <a href="#">MY DASHBOARD</a>
+              </div>
+            </div>
+            {/* secondary */}
+            <div className="flex gap-6">
+              <div className="hidden lg:flex items-center gap-5">
+                <div className="hidden lg:flex items-center gap-2">
+                  <MoonIcon className="h-6 w-6 " />
+                  <SunIcon className="h-6 w-6" />
+                </div>
+                <div>
+                  <button className="rounded-lg border-solid border-2 border-[#E94339] py-1 px-4 hover:bg-[#E94339] hover:text-white">
+                    LOGIN
+                  </button>
+                </div>
+              </div>
+              {/* Mobile navigation toggle */}
+              <div className="lg:hidden flex items-center">
+                <button onClick={() => setToggleMenu(!toggleMenu)}>
+                  <Bars3Icon className="h-6" />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <a className="btn btn-ghost normal-case text-2xl">Jashore Foodies</a>
-      </div>
-      <div className="navbar-center hidden lg:flex text-sm">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>HOME</a></li>
-          <li> <a>RESTAURANTS</a> </li>
-          <li><a>FOOD ITEMS</a></li>
-          <li><a>MY DASHBOARD</a></li>
-        </ul>
-      </div>
-      <div className="navbar-end text-sm">
-        <ul className="menu menu-horizontal px-1">
-          <li><div className=' border bg-[#2A435D] p-[8px_10px] text-[20px]  rounded-[300px] mr-2'>
-              <span className='text-white'><BsCart3></BsCart3></span>
-            </div></li>
-          <li tabIndex={0}>
-            <details>
-              <summary className="p-0 border-2 border-[#E94339]"><a className="py-2 px-3 rounded-sm " >LOGIN</a></summary>
-              <ul className="p-2">
-                <li><a>Customer</a></li>
-                <li><a>Restaurant</a></li>
-                <li><a>Delivery Man</a></li>
-              </ul>
-            </details>
-          </li>
-         <li><div className=' border bg-[#2A435D] p-[8px_10px] rounded-[300px] ml-2 text-[20px]'>
-              <span className='text-white'><MdOutlineDarkMode></MdOutlineDarkMode></span>
-            </div></li>
-        </ul>
-      </div>
+        {/* mobile navigation */}
+        <div
+          className={`fixed z-40 w-40  bg-gray-100 overflow-hidden flex flex-col lg:hidden gap-12  origin-top duration-700  ${!toggleMenu ? "h-0" : "h-2/3"
+            }`}
+        >
+          <div className="px-8">
+            <div className="flex flex-col gap-8 font-bold tracking-wider">
+              <a href="#" className="border-l-4 border-gray-600">
+                Features
+              </a>
+              <a href="#">Pricing</a>
+              <a href="#">Download</a>
+              <a href="#">Classic</a>
+              <MoonIcon className="h-6 w-6 " />
+              <SunIcon className="h-6 w-6" />
+              <button className="rounded-lg border-solid border-2 border-[#E94339] py-1 px-4 hover:bg-[#E94339] hover:text-white">
+                LOGIN
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
+  );
+};
 
-  )
-}
-
-export default NavBar
+export default NavBar;
